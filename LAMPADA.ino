152c89c237fa0570da62f500a509f721e4c8c1e7
Pedro Franco <101286154+PedroFerreiraFranco@users.noreply.github.com>
2022-06-03 14:41:49 -0300

    Update LAMPADA.ino

--- LAMPADA.ino
+++ LAMPADA.ino
@@ -1,55 +1,39 @@
-#include <ESP8266WiFi.h>   
+#ifdef ENABLE_DEBUG
+   #define DEBUG_ESP_PORT Serial
+   #define NODEBUG_WEBSOCKETS
+   #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#ifdef ESP8266 
+   #include <ESP8266WiFi.h>
+#endif 
+#ifdef ESP32   
+   #include <WiFi.h>
+#endif
+
 #include "SinricPro.h"
 #include "SinricProSwitch.h"
 
-#define WIFI_SSID         "iPhone de Pedro"    
-#define WIFI_PASS         "FyuB-ZXQo-6GJD-Caqp"
+#define WIFI_SSID         "Paiva"
+#define WIFI_PASS         "gabriel555"
 #define APP_KEY           "259f50d7-3382-4b5b-8511-26e452db7453"
-#define APP_SECRET       "a34a4f24-575d-4f7c-9916-303fa8d960fe-d90305fe-fcfd-44a3-a7ea-c2439d2195e5"
+#define APP_SECRET        "a34a4f24-575d-4f7c-9916-303fa8d960fe-d90305fe-fcfd-44a3-a7ea-c2439d2195e5"
 
-#define LampadaQuarto_ID      "62755aeb1d6a67083b4af45b"
-#define LampadaQuarto D2  // O pino fisico onde está ligado
-#define LampadaSala_ID      "627ea3daa6872f3639273014"
-#define LampadaSala D3 // O pino fisico onde está ligado
+#define SWITCH_ID_1       "62755aeb1d6a67083b4af45b"
+#define RELAYPIN_1        16
 
-#define BAUD_RATE         9600                // Se precisar, pode trocar o baud rate
+#define BAUD_RATE         115200                // Change baudrate to your need
 
-
-void setupWiFi();
-void setupSinricPro();
-
-
-// main setup function
-void setup() {
-  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
-  setupWiFi();
-  setupSinricPro(); 
-  pinMode(LampadaSala, OUTPUT);
-   pinMode(LampadaQuarto, OUTPUT);
-   
+bool onPowerState1(const String &deviceId, bool &state) {
+ Serial.printf("Device 1 turned %s", state?"on":"off");
+ digitalWrite(RELAYPIN_1, state ? HIGH:LOW);
+ return true; // request handled properly
 }
 
-void loop() {
-  SinricPro.handle();
-}
-
-bool LampadaState(const String &deviceId, bool &state) {
-  Serial.printf("A lampada foi %s\r\n", state?"ligada":"desligada");
-  digitalWrite(LampadaQuarto, state);
-  return true; // request handled properly
-
-}
-
-bool LampadaState2(const String &deviceId, bool &state) {
-  Serial.printf("A lampada foi %s\r\n", state?"ligada":"desligada");
-  digitalWrite(LampadaSala, state);
-  return true; // request handled properly
-
-}
-
-// setup das conexões Wifi
+// setup function for WiFi connection
 void setupWiFi() {
-  Serial.printf("\r\n[Wifi]: Conectando...");
+  Serial.printf("\r\n[Wifi]: Connecting");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -57,21 +41,32 @@ void setupWiFi() {
     delay(250);
   }
 
-  Serial.printf("Conectado!\r\n[WiFi]: Endereço de IP e %s\r\n", WiFi.localIP().toString().c_str());
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
 }
 
-// setup das funções para o SinricPro
+// setup function for SinricPro
 void setupSinricPro() {
   // add devices and callbacks to SinricPro
-
-  SinricProSwitch &mySwitch1 = SinricPro[LampadaQuarto_ID];
-  mySwitch1.onPowerState(LampadaState);
-
-    SinricProSwitch &mySwitch2 = SinricPro[LampadaSala_ID];
-  mySwitch2.onPowerState(LampadaState2);
-
+  pinMode(RELAYPIN_1, OUTPUT);
+    
+  SinricProSwitch& mySwitch1 = SinricPro[SWITCH_ID_1];
+  mySwitch1.onPowerState(onPowerState1);
+  
+  
   // setup SinricPro
-  SinricPro.onConnected([](){ Serial.printf("Conectado a nuvem SinricPro\r\n"); }); 
-  SinricPro.onDisconnected([](){ Serial.printf("Desconectado a nuvem SinricPro\r\n"); });
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+   
   SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+// main setup function
+void setup() {
+  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  SinricPro.handle();
 }
